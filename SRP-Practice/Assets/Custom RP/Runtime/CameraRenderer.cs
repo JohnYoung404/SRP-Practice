dd391560960c3ab41ee359758004e5f3122576ee
I5-5420\Administrator <JohnYoung404@outlook.com>
2022-08-25 11:33:36 +0800

    finish camera setup. An swap of two lines of code can reduce draw call from 2 to 1, which is interesting.

---
+++ SRP-Practice/Assets/Custom RP/Runtime/CameraRenderer.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class CameraRenderer
+{
+    ScriptableRenderContext context;
+    Camera camera;
+
+    const string bufferName = "Render Camera";
+    CommandBuffer buffer = new CommandBuffer
+    {
+        name = bufferName
+    };
+
+    public void Render(ScriptableRenderContext context, Camera camera)
+    {
+        this.context = context;
+        this.camera = camera;
+
+        Setup();
+        DrawVisibleGeometry();
+        Submit();
+    }
+
+    void Setup()
+    {
+        context.SetupCameraProperties(camera);
+        buffer.ClearRenderTarget(true, true, Color.clear);
+        buffer.BeginSample(bufferName);
+        ExecuteBuffer();
+    }
+
+    void DrawVisibleGeometry()
+    {
+        context.DrawSkybox(camera);
+    }
+
+    void Submit()
+    {
+        buffer.EndSample(bufferName);
+        ExecuteBuffer();
+        context.Submit();
+    }
+
+    void ExecuteBuffer()
+    {
+        context.ExecuteCommandBuffer(buffer);
+        buffer.Clear();
+    }
+}
---
+++ SRP-Practice/Assets/Custom RP/Runtime/CustomRenderPipeline.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class CustomRenderPipeline : RenderPipeline 
+{
+    CameraRenderer renderer = new CameraRenderer();
+
+    protected override void Render(ScriptableRenderContext context, Camera[] cameras)
+    {
+        foreach (Camera camera in cameras)
+        {
+            renderer.Render(context, camera);
+        }
+    }
+}
